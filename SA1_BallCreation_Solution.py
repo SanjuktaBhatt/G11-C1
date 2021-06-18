62ae7a046faa82a1601f770b4f65074f2aa03263
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-06-18 11:27:58 +0530

    Create SA1_BallCreation_Solution.py

---
+++ SA1_BallCreation_Solution.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((400,600))
+
+paddle=pygame.Rect(200,500,30,30)
+ball=pygame.Rect(70,50,10,10)
+
+while True:    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    pygame.draw.rect(screen,(23,100,100),paddle)
+    pygame.draw.rect(screen,(255,255,255),ball)
+    
+    pygame.display.update()
